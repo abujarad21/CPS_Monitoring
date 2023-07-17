4907bf51fef12e802f5cebcefc21bc95ef9dd63c
abujarad21 <abujarad73@gmail.com>
2023-07-17 10:44:39 +0800

    rn

--- django project/influxdb2mysqldb_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Influxdb2MysqldbAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'influxdb2mysqldb_app'
--- django project/influxdb2mysqldb_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import robots_view
-
-urlpatterns = [
-    #path('view_data/', view_data, name='view_data'),
-    path('robots/', robots_view, name='robots_view'),
-]
